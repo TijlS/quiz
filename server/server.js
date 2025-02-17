2c573c2941765412c9aa2133ded5b9bf809875a1
Tijl Schipper <tijl.schipper@gmail.com>
2025-02-17 16:43:45 +0100

    fix errors
    Signed-off-by: Tijl Schipper <tijl.schipper@gmail.com>

--- server/server.js
+++ server/server.js
@@ -4,16 +4,16 @@ const express = require("express");
 const app = express();
 
 const { createServer } = require("http");
-const httpServer = createServer(app)
+const httpServer = createServer(app);
 
 const { Server } = require("socket.io");
-const io = new Server(httpServer)
+const io = new Server(httpServer);
 
 const { v4: uuidv4 } = require("uuid");
 const cookieParser = require("cookie-parser");
 let questions = require("./questions.json");
 
-const basicAuth = require('express-basic-auth')
+const basicAuth = require("express-basic-auth");
 
 app.set("views", "./server/views");
 app.set("view engine", "ejs");
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 let players = [];
 let adminSocket = "";
 let currentQuestion = 0;
-let voted = 0
+let voted = 0;
 
 const changeScore = (value) => {
 	for (var i in players) {
@@ -33,7 +33,7 @@ const changeScore = (value) => {
 			break;
 		}
 	}
-}
+};
 
 const getScore = (value) => {
 	for (var i in players) {
@@ -41,7 +41,7 @@ const getScore = (value) => {
 			return players[i].score;
 		}
 	}
-}
+};
 
 const getTopUsers = () => {
 	players.sort((a, b) =>
@@ -49,21 +49,24 @@ const getTopUsers = () => {
 	);
 
 	return players.slice(0, 3);
-}
+};
 
-app.get('/restart_server', (req, res) => {
-	if(req.query.code !== "IwOsj8fK7jLt4EeyXPdiow==") return res.status(403).send('ERR: NOT AUTHENTICATED')
+app.get("/restart_server", (req, res) => {
+	if (req.query.code !== "IwOsj8fK7jLt4EeyXPdiow==")
+		return res.status(403).send("ERR: NOT AUTHENTICATED");
 
-	players = []
-	adminSocket = ""
-	currentQuestion = 0
+	players = [];
+	adminSocket = "";
+	currentQuestion = 0;
 
-	questions = require('./questions.json')
+	questions = require("./questions.json");
 
-	console.warn('SERVER RESTARTING')
+	console.warn("SERVER RESTARTING");
 
-	res.send('<CENTER><h1 style="color: yellow">SERVER RESTARTING...</h1></CENTER>')
-})
+	res.send(
+		'<CENTER><h1 style="color: yellow">SERVER RESTARTING...</h1></CENTER>'
+	);
+});
 
 app.get("/", (req, res) => {
 	res.render("index", {
@@ -71,10 +74,10 @@ app.get("/", (req, res) => {
 	});
 });
 app.post("/", (req, res) => {
-    const player = {
-        ...req.body,
-        score : 0
-    }
+	const player = {
+		...req.body,
+		score: 0,
+	};
 	players.push(player);
 
 	io.to(adminSocket).emit("user-joined", { name: player.name });
@@ -93,12 +96,14 @@ app.get("/game", (req, res) => {
 });
 app.get("/game/question/:id", (req, res) => {
 	res.render("game/question", {
-		question: questions[req.params.id] ? JSON.parse(JSON.stringify(questions[req.params.id])) : undefined,
-		number: parseInt(req.params.id)
+		question: questions[req.params.id]
+			? JSON.parse(JSON.stringify(questions[req.params.id]))
+			: undefined,
+		number: parseInt(req.params.id),
 	});
 });
 app.get("/game/answer/:id/:correct", (req, res) => {
-	voted = 0
+	voted = 0;
 	res.render("game/answer", {
 		question: questions[req.params.id],
 		correct: req.params.correct,
@@ -107,50 +112,49 @@ app.get("/game/answer/:id/:correct", (req, res) => {
 	});
 });
 app.post("/game/question/:id", (req, res) => {
-	let correct = false
-    const question = questions[req.params.id]
-	const answer = req.body.answer
-	voted++
+	let correct = false;
+	const question = questions[req.params.id];
+	const answer = req.body.answer ?? false;
+	voted++;
 
-	io.to(adminSocket).emit('update_voted', {
+	io.to(adminSocket).emit("update_voted", {
 		voted,
-		playerCount: players.length
-	})
+		playerCount: players.length,
+	});
 
-
-    switch (question.questionType) {
-        case "select":
-        case "image":
-            if (question.answers[answer].correct === true) {
-                correct = true;
-                changeScore(req.cookies.uuid);
-            }
-            break;
-        case "txtarea":
-            const containsKeywords = question.keywords.every(keyword => {
-				return answer.toLowerCase().includes(keyword)
-			})
-
-			if(containsKeywords) {
-				correct = true
+	switch (question.questionType) {
+		case "select":
+		case "image":
+			if (question.answers[answer].correct === true) {
+				correct = true;
 				changeScore(req.cookies.uuid);
 			}
-            break;
+			break;
+		case "txtarea":
+			const containsKeywords = question.keywords.every((keyword) => {
+				return answer.toLowerCase().includes(keyword);
+			});
+
+			if (containsKeywords) {
+				correct = true;
+				changeScore(req.cookies.uuid);
+			}
+			break;
 		case "txt":
-			if (answer.toLowerCase().includes(question.answer.toLowerCase())){
-				correct = true
+			if (answer.toLowerCase().includes(question.answer.toLowerCase())) {
+				correct = true;
 				changeScore(req.cookies.uuid);
 			}
 			break;
 		case "drag":
-			if (question.answer.join(';').toLowerCase() == answer){
-				correct = true
+			if (question.answer.join(";").toLowerCase() == answer) {
+				correct = true;
 				changeScore(req.cookies.uuid);
 			}
 
 			break;
-    }
-	
+	}
+
 	res.render("game/wait", {
 		question: question,
 		id: req.params.id,
@@ -165,21 +169,28 @@ app.get("/game/podium", (req, res) => {
 });
 
 //CONTROL
-app.get("/admin", basicAuth({
-	challenge: true,
-	users: { 
-		'admin': 'Tijl7965.'
-	}
-}), (req, res) => {
-	if(adminSocket !== "") return res.status(400).send('<CENTER><h1>NOT AUTHORIZED</h1></CENTER>')
+app.get(
+	"/admin",
+	basicAuth({
+		challenge: true,
+		users: {
+			admin: "Tijl7965.",
+		},
+	}),
+	(req, res) => {
+		if (adminSocket !== "")
+			return res
+				.status(400)
+				.send("<CENTER><h1>NOT AUTHORIZED</h1></CENTER>");
 
-	res.render("admin/index");
-});
+		res.render("admin/index");
+	}
+);
 
 io.on("connection", (socket) => {
-    socket.on('register_admin', () => {
-        adminSocket = socket.id
-    })
+	socket.on("register_admin", () => {
+		adminSocket = socket.id;
+	});
 
 	socket.on("start-game-server", () => {
 		socket.broadcast.emit("start-game", true);
@@ -191,13 +202,13 @@ io.on("connection", (socket) => {
 		currentQuestion++;
 	});
 
-    socket.on('get_game_info', () => {
-        let gameInfo = {
-            questionLength : questions.length
-        }
+	socket.on("get_game_info", () => {
+		let gameInfo = {
+			questionLength: questions.length,
+		};
 
-        socket.emit('game_info', gameInfo)
-    })
+		socket.emit("game_info", gameInfo);
+	});
 });
 
 httpServer.listen(PORT, () => {
